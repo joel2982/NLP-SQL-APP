5e61887c379d1b564506a7a0ed378737a32b2155
joel2982 <jjoel9958@gmail.com>
2023-11-07 04:15:14 +0530

    cleaned code

--- app.py
+++ app.py
@@ -39,42 +39,23 @@ def retrieve_names(cmd):
 
 def main():
     load_dotenv()
+    st.header('Natural Language to MS-SQL Queries')
+    #connecting to server
     driver = os.getenv('Driver')
     server = os.getenv('Server')
     db_name = os.getenv('db_name')
-
-    # username = os.getenv('username')
-    # password = os.getenv('password')
-    # server = os.getenv('server')
-    # db_name = os.getenv('db_name')
-    st.header('Natural Language to MS-SQL Queries')
-    # # connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={db_name};UID={username};PWD={password}'
-    # connectionString = 'DRIVER=ODBC Driver 18 for SQL Server;SERVER=DESKTOP-E20ERU;DATABASE=AdventureWorks2022;UID=Joel John;PWD=pass1231'
-    # connection = pyodbc.connect(connectionString) 
-
-    # connection_string=f'''
-    #     DRIVER={{{driver}}};
-    #     SERVER={server};
-    #     DATABASE={db_name};
-    #     trusted_connection=yes'''
-
     db = pyodbc.connect(f"DRIVER={driver};SERVER={server};DATABASE={db_name};trusted_connection=yes")      
     cursor=db.cursor()
-
+    # table and column selection for SQL querying 
     table_cmd = cursor.execute("SELECT concat (Table_schema,'.',Table_name) FROM information_schema.tables")
     table_names = retrieve_names(table_cmd)
-    selected_table = st.selectbox("Select a Table",options = table_names)
+    selected_table = st.selectbox("Select a Table",options=table_names,placeholder='Choose your option')
     if selected_table:
         column_cmd = cursor.execute(f"SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{selected_table.split('.')[1]}'")
         column_names = retrieve_names(column_cmd)
         selected_columns = st.multiselect("Select the Columns",options = column_names)
         st.write(selected_columns)
 
-    # engine = sqlalchemy.create_engine(f"mssql+pymssql://{username}:{password}@{server}.database.windows.net:1433/{db_name}", pool_recycle=3600)
-    # connection = pyodbc.connect(f"Driver={{{driver_name}}};Server={server_name};Database={db_name};Trusted_Connection=yes;")
-    # db = SQLDatabase.from_uri(f"mssql+pyodbc://{username}:{password}@{host}\SQLEXPRESS/DociQDb?driver=SQL+Server")
-    # db = SQLDatabase(engine)
-# DESKTOP-E20ERUT	AdventureWorks2022	1	53000002968800001	53000002971200001	53000002968800001	53000002467200001	23-05-2023 11:56:14	23-05-2023 11:56:15	209805312	DESKTOP-E20ERUT\rw	
 if __name__ == "__main__":
     main()
     
